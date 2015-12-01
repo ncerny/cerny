51bd2bf06260ef32ec759aeed815f307ed7111f3
Nathan Cerny <ncerny@chef.io>
2015-11-30 18:12:57 -0600

    Depends on delivery_build

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'ncerny@gmail.com'
 license 'apache2'
 description 'Cookbook for Maintaining Cerny Infrastructure'
 long_description 'Installs and maintains my personal infrastructure'
-version '0.5.2'
+version '0.5.3'
 
 supports 'redhat'
 
@@ -15,3 +15,4 @@ depends 'chef-vault'
 depends 'chef-analytics'
 depends 'supermarket-omnibus-cookbook'
 depends 'push-jobs'
+depends 'delivery_build'
