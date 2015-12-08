cfbe0d76f21f942577be044ec2d63732aa8fa48d
Nathan Cerny <ncerny@chef.io>
2015-12-08 13:24:04 -0600

    Create consul group before creating user.

--- recipes/consul.rb
+++ recipes/consul.rb
@@ -15,6 +15,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+group node['consul']['service_group']
+
 user node['consul']['service_user'] do
   group node['consul']['service_group']
   shell '/sbin/nologin'
